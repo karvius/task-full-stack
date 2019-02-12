a5fa691b8248d4f80095eb1cd11b9ef9e0645b0b
Rokas Grigaravicius <grigroka@gmail.com>
2019-02-12 17:18:39 +0200

    register  PlaceholderApiService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('PlaceholderApiService', function () {
+            return new App\Services\PlaceholderApiService();
+        });
     }
 
     /**
